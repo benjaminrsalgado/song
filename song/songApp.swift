6989be42dbc02dcda94c9e5ae9a800a3ee081731
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-07 16:28:51 -0600

    Initial Commit

---
+++ song/songApp.swift
@@ -0,0 +1,17 @@
+//
+//  songApp.swift
+//  song
+//
+//  Created by Benjamin Rojo on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct songApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
