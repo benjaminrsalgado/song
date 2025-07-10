bb9b2abacc3da43bf12f7900eb3e4567c1ae6a67
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-10 12:22:44 -0600

    i practice all about @

--- song/ContentView.swift
+++ song/ContentView.swift
@@ -9,12 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-
+        PantallaPrincipal()
+       
     }
 }
 
+//ESTA ES LA CALSE
 class  ContadorModel: ObservableObject{
     @Published var cuenta = 0
+    
+    func change(){
+        cuenta += 1
+    }
+}
+//LA VISTA QUE HEREDA LA CLASE
+struct PantallaPrincipal: View{
+    @StateObject var keep = ContadorModel()
+    var body: some View{
+        Text("\(keep.cuenta)")
+        BotonContador(keep: keep)
+    }
+}
+//LA SEGUNDA VISTA HEREDANDO LA CLASE
+struct BotonContador:View{
+    @ObservedObject var keep: ContadorModel
+    var body: some View{
+        Button(action:{
+            keep.change()
+        }){Text("Press the button")
+                .foregroundColor(.red)
+        }
+    }
 }
 
 
