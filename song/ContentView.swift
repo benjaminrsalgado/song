120a7684a8a7f17ca5252e8fc6d903f8b2391d8d
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-18 17:26:02 -0600

    i add clases and structs

--- song/ContentView.swift
+++ song/ContentView.swift
@@ -42,6 +42,35 @@ struct TareaView: View{
         })
     }
 }
+struct Tarea: Identifiable{
+    let id = UUID()
+    let titulo : String
+    let completada : Bool
+}
+
+class TareasManager{
+    @Published var tareas: [Tarea]
+    
+    func change(){
+        tareas.toggle
+    }
+}
+
+struct ListaDeTareas: View{
+    @StateObject var mostrar = TareasManager()
+    var body: some View{
+        for(mostrar)
+    }
+}
+
+struct TareaView: View{
+    @ObservedObject var mostrar: TareasManager
+    var body some view{
+        Button(action:{
+            
+        })
+    }
+}
 #Preview {
     ContentView()
 }
